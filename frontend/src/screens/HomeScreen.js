4039d2225a92a8b75a6551f9ea49c9fa1c0168ee
HaudegenHH <einsundeins79@gmx.de>
2022-04-06 15:04:24 +0200

    Installing Axios / Fetch products from backend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,12 +1,24 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
-import data from '../data';
 
 function HomeScreen() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await axios.get('/api/products');
+      setProducts(response.data);
+      //console.log(products);
+    };
+    fetchProducts();
+  }, []);
+
   return (
     <>
       <h1>Featured Products</h1>
       <div className="products">
-        {data.products.map((product) => (
+        {products.map((product) => (
           <div className="product" key={product.name}>
             <Link to={`/product/${product.slug}`}>
               <img src={product.image} alt={product.name} />
